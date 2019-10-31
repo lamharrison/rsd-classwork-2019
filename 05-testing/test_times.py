f08910003c9e4b77afea172c272c3a5e11812664
lamharrison <lamharrison123@gmail.com>
2019-10-31 11:23:39 +0000

    add test class time

--- 05-testing/test_times.py
+++ 05-testing/test_times.py
@@ -6,3 +6,10 @@ def test_given_input():
     result = overlap_time(large, short)
     expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
     assert result == expected
+
+def test_class_time():
+    large = time_range("2010-10-31 10:00:00", "2010-10-31 13:00:00")
+    short = time_range("2010-10-31 10:05:00", "2010-10-31 12:55:00", 3, 600)
+    result = overlap_time(large, short)
+    expected = [('2010-10-31 10:05:00', '2010-10-31 10:55:00'), ('2010-10-31 11:05:00', '2010-10-31 11:55:00'),('2010-10-31 12:05:00', '2010-10-31 12:55:00')]
+    assert result == expected
