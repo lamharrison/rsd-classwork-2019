7d5201a128d2663849ac671e7ea8a92c6b364293
lamharrison <lamharrison123@gmail.com>
2019-10-31 11:43:31 +0000

    add questions

--- 05-testing/test_times.py
+++ 05-testing/test_times.py
@@ -11,5 +11,11 @@ def test_class_time():
     large = time_range("2010-10-31 10:00:00", "2010-10-31 13:00:00")
     short = time_range("2010-10-31 10:05:00", "2010-10-31 12:55:00", 3, 600)
     result = overlap_time(large, short)
-    expected = [('2010-10-31 10:05:00', '2010-10-31 10:55:00'), ('2010-10-31 11:05:00', '2010-10-31 11:55:00'),('2010-10-31 12:05:00', '2010-10-31 12:55:00')]
+    expected = short  #test it without running it add
     assert result == expected
+
+#what happens if large times is smaller than short times?
+#what happens if the date is not exist, for example 29/02/leap year
+#what happens with different dates, examples dates are same dates
+#what happens if time range is in opposite order
+#how to express for AD years?
