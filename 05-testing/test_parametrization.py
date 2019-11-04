07a84b73a40f3988cca8703b9b893a637f59d1a5
lamharrison <lamharrison123@gmail.com>
2019-11-04 11:34:28 +0000

    UCL-RITS#33
    x
    
    exit

---
+++ 05-testing/test_parametrization.py
@@ -0,0 +1,10 @@
+import pytest
+from times import overlap_time, time_range
+
+@pytest.mark.parametrize("large, short, expected", [(time_range("2010-01-12 00:00:00", "2010-01-12 23:50:00", 2, 600),
+                                                     time_range("2010-01-13 00:00:00", "2010-01-13 23:50:00", 2, 600),
+                                                     [])] )
+
+def test_eval(large, short, expected):
+    assert (overlap_time(large, short)) == expected
+
