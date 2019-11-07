fcabccdc7165650d1d235875bc2291907d353669
lamharrison <lamharrison123@gmail.com>
2019-11-07 19:07:08 +0000

    some changes

--- 05-testing/testing_edges_touching.py
+++ 05-testing/testing_edges_touching.py
@@ -2,14 +2,12 @@ from times import overlap_time, time_range
 
 
 def test_given_input():
-    # large is a range of one hour length with 10 min blocks and 10 min gaps
-    # short is a range of one hour length with 10 min blocks and 10 min gaps
-    # but it starts 10 mins later than large
+
     large = time_range("2010-01-12 00:00:00", "2010-01-12 00:50:00", 3, 600)
     short = time_range("2010-01-12 00:10:00", "2010-01-12 01:00:00", 3, 600)
 
     result = (overlap_time(large, short))
-    # there should be no overlap, should return empty tuple
+
 
     expected =  [('2010-01-12 00:10:00', '2010-01-12 00:10:00'),
           ('2010-01-12 00:20:00', '2010-01-12 00:20:00'),
